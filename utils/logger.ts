7bd4598cef899de3c64af1a8218efd89feffd0b1
hykimst <hykim@matterport.com>
2025-05-21 22:11:45 -0400

    remove addelement, never used

--- utils/logger.ts
+++ utils/logger.ts
@@ -7,24 +7,3 @@ export function LOG(msg: string, ...payload: any) {
   export function ERROR(msg: string, ...payload: any) {
     console.log(`%c === ${msg} ===`, 'background:#FF0000; color:white;', payload);
   }
-
-  export function ADDELEMENT(
-    parentNode: Element,
-    element: string,
-    msg:any,
-    ...payload: any
-  ) {
-    const node = document.createElement(element);
-    if (payload !== null && parent) {
-      let time = new Date().toLocaleString("en-US", { timeZone: "EST" })
-      node.innerHTML = `${time}&emsp;${msg}  ${payload}`;
-    }
-    console.log(
-      `%c === CREATE ${element} ===`,
-      'background:#FF0000; color:white;',
-      payload
-    );
-    parentNode.insertBefore(node, parentNode.firstChild);
-    return parentNode;
-  }
-  
