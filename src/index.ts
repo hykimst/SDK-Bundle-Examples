7e191be7ec07a739820c49e3a79697078afd0a4a
hykimst <hykim@matterport.com>
2025-05-21 21:56:10 -0400

    mp.transform example

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
-import { myOpenTagsAndHover } from "./myOpenTagsHover";
-import { myRegisterAttachment } from "./registerAttachment";
+import { DEBUG, LOG } from '../utils/logger';
+import transformtest from "./transformcomponent";
 
 const showcase = document.getElementById('showcase') as HTMLIFrameElement;
 const logsElement = document.getElementById('log-list') as HTMLElement;
 const key = 'yxszifc05b1bidcsqfr60806d';
-
-// declare this file is a module
-export {};
 
 // augment window with the MP_SDK property
 declare global {
@@ -17,18 +14,31 @@ declare global {
 showcase.addEventListener('load', async function () {
   try {
     await showcase.contentWindow.MP_SDK.connect(showcase, key, '3.6')
-      .then(onShowcaseConnect)
+      .then(MPTransformTest)
       .catch(console.error);
   } catch (e) {
     console.error(e);
     return;
   }
 
-  async function onShowcaseConnect(SDK: any) {
-    // Register Attachment Example
-    // myRegisterAttachment(SDK);
+  
+  let added = false;
+  let position ={x:0,y:0,z:0}
+  async function MPTransformTest(SDK: any) {
+    SDK.Camera.pose.subscribe((pose:any)=>{
+      // DEBUG("camera", pose);
+    })
+    SDK.Tag.data.subscribe({
+      onAdded(index: any, item: any, collection: any) {
+        if (!added) {
+          position=item.anchorPosition,
+          LOG('[Tag] onAdded', index, item, collection);
+          // MP Transform Test
+          transformtest(SDK, logsElement, position);
+          added = true;
+        }
+      },
+    });
 
-    // Hover & Open Example
-    myOpenTagsAndHover(SDK,logsElement);
   }
 });
--- utils/logger.ts
+++ utils/logger.ts
@@ -1,7 +1,4 @@
 export function LOG(msg: string, ...payload: any) {
-    if (payload === null) {
-      payload = 'no payload';
-    }
     console.log(`%c === ${msg} ===`, 'background:#008000; color:white;', payload);
   }
   export function DEBUG(msg: string, ...payload: any) {
