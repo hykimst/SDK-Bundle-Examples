2b2c1f5654adf02e70a42451913bc3d1727dfe24
hykimst <hykim@matterport.com>
2025-05-21 22:05:06 -0400

    simplified index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,26 +12,10 @@ declare global {
 }
 showcase.addEventListener('load', async function () {
   try {
-    await showcase.contentWindow.MP_SDK.connect(showcase, key, '3.6')
-      .then(MPTransformTest)
-      .catch(console.error);
+    const mpSdk = await showcase.contentWindow.MP_SDK.connect(showcase, key, '3.6');
+    transformtest(mpSdk);
   } catch (e) {
     console.error(e);
     return;
-  }
-  
-  let added = false;
-  async function MPTransformTest(SDK: any) {
-    SDK.Tag.data.subscribe({
-      onAdded(index: any, item: any, collection: any) {
-        if (!added) {
-          LOG('[Tag] onAdded', index, item, collection);
-          // MP Transform Test
-          transformtest(SDK);
-          added = true;
-        }
-      },
-    });
-
   }
 });
